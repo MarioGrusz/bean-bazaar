2ad761a0d38cddc209a7bc9ec7a12cb0d1938d12
Mario <mario.gruszczynski@gmail.com>
2023-11-23 18:02:46 +0100

    google services updated

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -3,16 +3,16 @@ import * as dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Load environment variables from .env file
+//Load environment variables from .env file
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 // Use the GOOGLE_APPLICATION_CREDENTIALS environment variable
-const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK);
 
 admin.initializeApp({
- credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
 
 export default admin;
